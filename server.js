31afd69a0ce2559cf7b8c7f1e75dc2eb84d85c38
Huong <huongnguyenak96@gmail.com>
2019-05-09 20:24:35 +0700

    post add

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+.urlencoded({extended:false}); 
 const app = express();
 app.set('view engine','ejs')
+app.use(bodyParser)
 
 const { 
     Singer, 
@@ -15,6 +18,14 @@ app.get('/',(req,res)=>{
 
 app.get('/add-singer',(req,res)=>{
     res.render('add-singer');
+})
+app.post('/add-singer',(req,res)=>{
+    // const txtName = req.body.name;
+    const { name, link, avatar } = req.body
+    const id =  Math.floor(Math.random() * 100)
+    const singer = new Singer(id, name, link, avatar)
+    listSinger.unshift(singer);
+    res.redirect('/')
 })
 
 app.listen(3000);
